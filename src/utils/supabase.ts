57e76bd9ba2e383403e4b94a967ad81b11d399a3
John <codingjohn@outlook.com>
2025-12-17 17:18:24 +0800

    fix: fix bundling issue

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -7,8 +7,8 @@ const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false
+    autoRefreshToken: true as boolean,
+    persistSession: true as boolean,
+    detectSessionInUrl: false as boolean
   }
 })
